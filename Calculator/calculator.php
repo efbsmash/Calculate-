3ed58c47ca3e59d8eacebd7c3ac0be64d3d64003
d00phus <40159676+efbsmash@users.noreply.github.com>
2018-12-07 23:16:07 -0500

    Update calculator.php

--- Calculator/calculator.php
+++ Calculator/calculator.php
@@ -5,7 +5,7 @@
     <title>Calculate!</title>
 </head>
 <body>
-    <form action="" method="POST"></form>
+    <form action="calc.php" method="POST">
     <input type="text" name="num1">
     <input type="text" name="num2">
     <select name="calc">
@@ -14,5 +14,6 @@
         <option value="mul">Multiply</option>
     </select>
     <button type="submit">Calculate</button>
+    </form>
 </body>
 </html>
