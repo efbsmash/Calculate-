a8ffff4599251d869514a0be4cd0a6c04ac13ce6
d00phus <40159676+efbsmash@users.noreply.github.com>
2018-12-07 22:14:28 -0500

    Add files via upload

---
+++ Calculator/calc.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'inc/IncCalc.php';
+
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$calc = $_POST['calc'];
+
+$calculator = new Calc($num1, $num2, $calc);
+
+echo $calculator->calcMethod();
---
+++ Calculator/calculator.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Calculate!</title>
+</head>
+<body>
+    <form action="" method="POST"></form>
+    <input type="text" name="num1">
+    <input type="text" name="num2">
+    <select name="calc">
+        <option value="add">Add</option>
+        <option value="sub">Subtract</option>
+        <option value="mul">Multiply</option>
+    </select>
+    <button type="submit">Calculate</button>
+</body>
+</html>
